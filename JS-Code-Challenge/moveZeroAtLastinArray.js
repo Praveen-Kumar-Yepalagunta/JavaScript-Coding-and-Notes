ee1cb2bceb13a9a318e938ff614ddf2fb38c2e55
Praveen Kumar Yepalagunta <83775367+Praveen-Kumar-Yepalagunta@users.noreply.github.com>
2026-03-11 12:08:03 +0530

    Update moveZeroAtLastinArray.js

--- JS-Code-Challenge/moveZeroAtLastinArray.js
+++ JS-Code-Challenge/moveZeroAtLastinArray.js
@@ -1,22 +1,43 @@
 // input : [0,1,0,3,12]
 // output: [1,3,12,0,0]
 
-function transformArray(arr){
-    let result=[];
+// function transformArray(arr){
+//     let result=[];
 
-    for(let i=0; i<arr.length; i++){
-        if(!arr[i]==0){
-            result.push(arr[i]);
-        }
+//     for(let i=0; i<arr.length; i++){
+//         if(!arr[i]==0){
+//             result.push(arr[i]);
+//         }
+//     }
+
+//     let zeroLength = arr.length - result.length;
+//     for(let j=0;j<zeroLength;j++){
+//         result.push(0)
+//     }
+
+//     return result;
+// }
+
+
+function moveZeroToLastInArray(arr){
+  let result = [];
+
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] !== 0){
+      result.push(arr[i]);
     }
+  }
 
-    let zeroLength = arr.length - result.length;
-    for(let j=0;j<zeroLength;j++){
-        result.push(0)
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] === 0){
+      result.push(0);
     }
+  }
 
-    return result;
+  console.log(result);
 }
+
+moveZeroToLastInArray([0,1,0,3]);
 
 
 console.log(transformArray([0,1,0,3,12]));
